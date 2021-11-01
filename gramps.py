609217fd72c188ca34c8ce97126d091aaae5a702
larsnorberg <larsnorberg@hotmail.com>
2021-11-01 08:32:02 +0100

    drop table added

--- gramps.py
+++ gramps.py
@@ -6,15 +6,18 @@ con = sqlite3.connect('example.db')
 conNew = sqlite3.connect('grampsLN.db')
 cur = con.cursor()
 curLN = conNew.cursor()
-
-curLN.execute("CREATE TABLE IF NOT EXISTS person (Handle VARCHAR(50) PRIMARY KEY NOT NULL, given_name TEXT)")
+curLN.execute("DROP TABLE IF EXISTS person")
+curLN.execute("CREATE TABLE person (Handle VARCHAR(50) PRIMARY KEY NOT NULL, given_name TEXT)")
 
 for row in cur.execute("SELECT * FROM person WHERE gramps_id ='I0005' "):
     print(type(row[3]))
     p = pickle.loads((row[3]))
     print(type(p))
     print(p)
-    curLN.execute("INSERT INTO person values (?,?)", (row[0], row[1]))
+    record = (row[0],row[1])
+    curLN.execute("INSERT INTO person values (?,?)", record)
 
 conNew.commit()
 result = curLN.execute("SELECT * FROM person")
+print(result)
+print("Execution finished")
