435a4a70cbc7fd9f6fbdb582ecbce1a06ae38bf7
larsnorberg <larsnorberg@hotmail.com>
2021-11-01 08:05:28 +0100

    new database, checked insert into qmark style

--- gramps.py
+++ gramps.py
@@ -1,11 +1,20 @@
 import pickle
 import sqlite3
-msg = "hello world"
+msg = "Execution started"
 print(msg)
 con = sqlite3.connect('example.db')
+conNew = sqlite3.connect('grampsLN.db')
 cur = con.cursor()
+curLN = conNew.cursor()
+
+curLN.execute("CREATE TABLE IF NOT EXISTS person (Handle VARCHAR(50) PRIMARY KEY NOT NULL, given_name TEXT)")
+
 for row in cur.execute("SELECT * FROM person WHERE gramps_id ='I0005' "):
     print(type(row[3]))
     p = pickle.loads((row[3]))
     print(type(p))
     print(p)
+    curLN.execute("INSERT INTO person values (?,?)", (row[0], row[1]))
+
+conNew.commit()
+result = curLN.execute("SELECT * FROM person")
