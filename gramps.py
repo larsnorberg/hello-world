c88dd103d7b2eef0026d70c87ba412fc21e13ae7
larsnorberg <larsnorberg@hotmail.com>
2021-11-07 11:23:48 +0100

    ändring

--- gramps.py
+++ gramps.py
@@ -13,7 +13,7 @@ for row in cur.execute("SELECT * FROM person WHERE gramps_id ='I0005' "):
     print(type(row[3]))
     p = pickle.loads((row[3]))
     print(type(p))
-    print(p)
+    print(p[9])
     record = (row[0],row[1])
     curLN.execute("INSERT INTO person values (?,?)", record)
 
