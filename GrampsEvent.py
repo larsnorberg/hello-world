ffb828367e651670eb9a82b1c7bf8f87325e6e73
larsnorberg <larsnorberg@hotmail.com>
2021-11-26 07:47:51 +0100

    Lagt till analys av db-data

--- GrampsEvent.py
+++ GrampsEvent.py
@@ -8,17 +8,23 @@ conEx = sqlite3.connect('example.db')
 conLn = sqlite3.connect('grampsLN.db')
 curEx = conEx.cursor()
 sql="""
-   SELECT event.blob_data,event.gramps_id,event.description,person.Handle,person.given_name,person.surname 
+   SELECT event.blob_data,event.gramps_id,event.description,person.Handle,person.given_name,person.surname,place.title  
    FROM event
    JOIN reference ON event.handle = reference.ref_handle
    JOIN person ON reference.obj_handle = person.handle
+   LEFT JOIN place on event.place = place.handle
 """
 for eventRow in curEx.execute(sql):
     event = pickle.loads((eventRow[0]))
+    print(eventRow[1]) # gramps.id
+    print(eventRow[2]) # event.description
+    print(eventRow[3]) # Person.Handle
+    print(eventRow[4]) # person.given_name
+    print(eventRow[5]) # person.surname
+    print(eventRow[6]) # place.title
     print(event[1]) # eventID
     print(event[2]) # eventType 12=födelse, 13=död
     print(event[3]) # datum tuple
-    d = event[3]
-    print(d[3])
-    print(event[4]) # description
-    print(event[5]) # place.handle ?
+    d = event[3] # event.date
+    if d is not None:
+      print(d[3])
