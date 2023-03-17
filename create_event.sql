45344ff4cdad2308359249857af2ee411594ac5a
larsnorberg <larsnorberg@hotmail.com>
2023-03-17 08:43:21 +0100

    test och rättning

--- create_event.sql
+++ create_event.sql
@@ -1,4 +1,5 @@
-DROP TABLE IF EXISTS event_ex
+DROP TABLE IF EXISTS event_ex;
 CREATE TABLE event_ex (handle VARCHAR(50) PRIMARY KEY NOT NULL, gramps_id TEXT, date_str TEXT, description TEXT, place_handle VARCHAR(50), change_str TEXT, private INTEGER)
-DROP INDEX IF EXISTS event_gramps_id
-CREATE INDEX event_gramps_id ON event(gramps_id);
+DROP INDEX IF EXISTS event_gramps_id;
+CREATE INDEX event_gramps_id ON event_ex(gramps_id);
+INSERT INTO event_ex (handle, gramps_id) values ('abcd', 'E0001')
--- event_ex.py
+++ event_ex.py
@@ -13,16 +13,16 @@ print(msg)
 class eventCls: # class to handle event data and extension table
     def __init__(self, tuple): # tuple is the tuple returned from unpickle blob_data in event table
         self.change = datetime.datetime.fromtimestamp(tuple[10]).strftime('%Y-%m-%d %H:%M:%S')
-        self.date_str = tuple[3][4]
-        self.description = tuple[2]
-        self.event_type = event_types[tuple[2][0]]
+        self.date_str = tuple[3][4] if tuple[3] != None else ''
+        self.description = tuple[4]
+        self.event_type = event_types[tuple[2][0]] if len(event_types) > tuple[2][0] else str(tuple[2][0])
         self.gramps_id = tuple[1]
         self.handle = tuple[0]
         self.place_handle = tuple[5]
         self.private = tuple[12]       
     def __str__(self):
-        return f"{self.event_type} {self.description} {self.date_str} place:{self.place_handle}"
-    sql_insert_txt = "INSERT INTO event_ex (handle, gramps_id, date_str, description, place_handle, change, private) values (?,?,?,?,?,?,?)"
+        return f"{self.event_type} {self.description} {self.date_str} {self.gramps_id} place:{self.place_handle}"
+    sql_insert_txt = "INSERT INTO event_ex (handle, gramps_id, date, description, place_handle, change, private) values (?,?,?,?,?,?,?)"
     def exec_insert(self, cursor):
         private = 1 if self.private else 0
         exec_result = cursor.execute (self.sql_insert_txt, (self.handle, self.gramps_id, self.date_str, self.description, self.place_handle, self.change, private))
@@ -34,10 +34,10 @@ con = sqlite3.connect('example.db')
 cur = con.cursor()
 con_ex = sqlite3.connect('grampsLN.db')
 cur_ex = con_ex.cursor()
-for row in cur.execute("SELECT handle, blob_data, description FROM event LIMIT 10 "):
+for row in cur.execute("SELECT handle, blob_data, description FROM event"):
     p = pickle.loads((row[1]))
-    print("type(p):", type(p)) 
-    print_collection(p)
+    # print("type(p):", type(p)) 
+    # print_collection(p)
     event = eventCls(p)
     print(event)
     result = event.exec_insert(cur_ex)
