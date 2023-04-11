ec4180c1afbea2d4cf46342534b5a88caddf003e
larsnorberg <larsnorberg@hotmail.com>
2023-04-11 09:48:31 +0200

    add join from event to reference

--- event_ex.py
+++ event_ex.py
@@ -23,13 +23,14 @@ class Event: # class to handle event data and extension table
         self.gramps_id = tuple[1]
         self.handle = tuple[0]
         self.place_handle = tuple[5]
-        self.private = tuple[12]       
+        self.private = tuple[12]
     def __str__(self):
         return f"{self.event_type} {self.description} {self.date_str} {self.gramps_id} place:{self.place_handle}"
-    ### sql_insert_txt = "INSERT INTO event_ex (handle, gramps_id, date, event_type, description, place_title, change, private) values (?,?,?,?,?,?,?,?)"
-    sql_insert_txt = "INSERT INTO persons_event (handle, gramps_id, date, event_type, description, place_title, change, private) values (?,?,?,?,?,?,?,?)"
-    def exec_insert(self, con, place_title):
-        exec_result = con.execute (self.sql_insert_txt, (self.handle, self.gramps_id, self.date_str, self.event_type, self.description, place_title, self.change, self.private))
+    ### sql_insert_txt = "INSERT INTO event_ex (handle, gramps_id, date, event_type, description, place_title, change, private, obj_handle, obj_class) values (?,?,?,?,?,?,?,?,?,?)"
+    sql_insert_txt = "INSERT INTO persons_event (handle, gramps_id, date, event_type, description, place_title, change, private, obj_handle, obj_class) values (?,?,?,?,?,?,?,?,?,?)"
+    def exec_insert(self, con, place_title, obj_handle, obj_class):
+        exec_result = con.execute (self.sql_insert_txt, (self.handle, self.gramps_id, self.date_str, self.event_type, self.description, \
+                                                         place_title, self.change, self.private, obj_handle, obj_class)) 
         return exec_result
 # end Event
 
@@ -39,11 +40,13 @@ con = sqlite3.connect('file:sqlite.db?mode=ro', uri=True)
 con.row_factory = sqlite3.Row # use row_faktory
 ### con_ex = sqlite3.connect('grampsLN.db')
 con_ex = sqlite3.connect('C:\\Users\\larsn\\GitHub\\my-django\\my_gramps\\db.sqlite3')
-for row in con.execute("""SELECT event.handle, event.blob_data, place.title FROM event LEFT JOIN place ON event.place = place.handle """):
+for row in con.execute("""SELECT event.handle, event.blob_data, place.title, r.obj_handle, r.obj_class 
+        FROM event LEFT JOIN place ON event.place = place.handle
+         JOIN reference AS r ON r.ref_handle = event.handle"""):
     ### print_collection(pickle.loads(row[1])) # for debugging
     event = Event(pickle.loads(row[1]))
     print(event)
-    result = event.exec_insert(con_ex, row[2])
+    result = event.exec_insert(con_ex, row[2], row[3], row[4])
 con_ex.commit()
 con_ex.close()
 con.close()
