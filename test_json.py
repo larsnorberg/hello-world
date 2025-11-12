d2fcd5d50246094fc30d3966236b0cd7dc093c2d
larsnorberg <larsnorberg@hotmail.com>
2025-11-12 22:46:18 +0100

    analys av json i db

--- test_json.py
+++ test_json.py
@@ -1,6 +1,7 @@
 import json
 import sqlite3
 
+print("execution started\n")
 db_source_uri = 'file:///C:/Users/larsn/Google%20Drive/gramps6/6914604a/sqlite.db?mode=ro' 
 
 con = sqlite3.connect(db_source_uri, uri=True)
@@ -11,6 +12,11 @@ sql="""
 """
 res = con.execute(sql)
 person = res.fetchone()
-person.keys()
-pass
+print(person.keys())
+### print(person["json_data"])
+person_dict =json.loads(person["json_data"])
+for column in person_dict:
+    ###print(column,person_dict[column], type(person_dict[column]))
+    ...
 con.close()
+print('execution ended')
